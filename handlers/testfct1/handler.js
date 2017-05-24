dd5d5c69ce061fa1dca2955d3dc73bbc66785b6a
Frank Schmid <fschmid@stashimi.com>
2017-05-24 13:33:47 +0200

    Sample branch with warmup plugin, APIG and resource changes

--- handlers/testfct1/handler.js
+++ handlers/testfct1/handler.js
@@ -3,6 +3,12 @@
 import { Test1 } from '../../src/test1';
 
 export function handle(event, context) {
+
+	if (event.source === "serverless-plugin-warmup") {
+		console.log("Warmup request."); // eslint-disable-line no-console
+		return context.done(null, "Lambda is warm.");
+	}
+	
 	Test1.run(event).asCallback(context.done);
 	return; // no Promise return as we invoke `context.done` directly!
 }
